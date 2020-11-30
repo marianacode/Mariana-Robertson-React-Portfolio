28f91abf06271879f573f6da066d90df4724a4b3
Mariana Robertson <marygmaciel@gmail.com>
2020-11-29 21:37:57 -0800

    Updates

---
+++ precache-manifest.6b69d9e3301e474457890bf4fec705ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d121c08c9c201cbea9a5088ccf14306e",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/index.html"
+  },
+  {
+    "revision": "4f4202155caa450bcfdc",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "61048e6636a92ad9cb60",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/css/main.3d899b1e.chunk.css"
+  },
+  {
+    "revision": "4f4202155caa450bcfdc",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/js/2.09a09628.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/js/2.09a09628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61048e6636a92ad9cb60",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/js/main.af1966fe.chunk.js"
+  },
+  {
+    "revision": "8a944e523db7eda735c0",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/js/runtime-main.30212b9a.js"
+  },
+  {
+    "revision": "80e8c3d9245d318901029a1c08612f94",
+    "url": "/marianacode/Mariana-Robertson-React-Portfolio/static/media/Mariana Capture.80e8c3d9.PNG"
+  }
+]);
